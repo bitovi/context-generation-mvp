8f249b3c85333364141d90cfbc827b43cbfa167a
Wade H. <wade@bitovi.com>
2025-11-07 14:03:01 -0500

    Add claude-agent SDK example

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
+import { query } from '@anthropic-ai/claude-agent-sdk';
 import { Anthropic } from '@anthropic-ai/sdk';
 import { loadEnvFile } from 'node:process';
 
 loadEnvFile('.env');
 
+// Using anthropic SDK
 const anthropic = new Anthropic({
     apiKey: process.env.ANTHROPIC_API_KEY!,
 });
@@ -15,3 +17,7 @@ const msg = await anthropic.messages.create({
 console.log(msg);
 
 
+// Using claude agent sdk
+for await(const qMsg of query({prompt: "Hello, Claude"})) {
+    console.log(qMsg)
+};
