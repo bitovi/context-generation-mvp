5193e8357168f3c315344b7599263e81c64cdfdd
Wade H. <wade@bitovi.com>
2025-11-07 13:58:49 -0500

    formatting

--- index.ts
+++ index.ts
@@ -1,21 +1,17 @@
-import {Anthropic} from '@anthropic-ai/sdk';
+import { Anthropic } from '@anthropic-ai/sdk';
 import { loadEnvFile } from 'node:process';
 
 loadEnvFile('.env');
-
-
 
 const anthropic = new Anthropic({
     apiKey: process.env.ANTHROPIC_API_KEY!,
 });
 
-async function main() {
-  const msg = await anthropic.messages.create({
+const msg = await anthropic.messages.create({
     model: "claude-sonnet-4-5",
     max_tokens: 1024,
     messages: [{ role: "user", content: "Hello, Claude" }],
-  });
-  console.log(msg);
-}
+});
+console.log(msg);
 
-main();
+
